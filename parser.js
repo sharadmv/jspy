9fd26de3919341c924b59ce6b66f573ce3880a55
Stephen Martinis <moowiz2020@gmail.com>
2012-10-24 10:45:33 -0700

    First commit on workingon the parser.

--- parser.js
+++ parser.js
@@ -5,6 +5,29 @@ var Parser = function () {
         return tokens;
     }
 }
+
+var Kinds = {
+    DEF: 0,
+    ID: 1,
+    INDENT: 2,
+    DEDENT: 3,
+    OP: 4,
+    IF: 5,
+    ELSE: 6,
+    ELIF: 7,
+    ERR: 8
+}
+
+var Identifiers = {
+    "def" : false,
+    "if" : false,
+    "else" : false,
+    "elif" : false,
+    "return" : false,
+    "while" : false,
+    "for" : false,
+}
+
 var SPLITS = {
     "+": {"+=" : false},
     "-": {"-=" : false},
@@ -40,54 +63,103 @@ var SPLITS = {
     ",":false,
     ":":false
 }
+
 var Tokenizer = function(text) {
     var TOKEN_SPLITS = { " " : false }
     var chunked = [];
     var build = "";
+    var id_begin = /[A-Za-z_]/;
+    var id_inside = /\w/;
     text = text.trim();
-    for (var i = 0; i < text.length; i++) {
+    var i = 0;
+    var is_number = function(text) {
+        return /[0-9]+/g.exec(text);
+    }
+    var current = function(){
+        return text.charAt(i);
+    }
+    var consume = function(){
+        var rval = text.charAt(i);
+        i ++;
+        return rval;
+    }
+    var accept = function(all_allowed){
         var char = text.charAt(i);
-        var curOps = SPLITS;
-        var token = char;
-        curOps = SPLITS;
-        if (token in curOps) {
-            build = build.trim();
-            if (build != "") {
-                chunked.push(build);
-                build = "";
+        var any = false;
+
+        if(i - 1 == text.length){
+            return false;
+        }
+
+        for(var j = 0; j < all_allowed.length; j ++){
+            if(char == all_allowed.charAt(j)){
+                build += char;
+                i ++;
+                char = text.charAt(i);
+                any = true;
             }
+        }
+        return any;
+    }
+    var accept_regex = function(reg){
+        if(i - 1 == text.length){
+            console.log('done');
+            return false;
+        }
+        var char = text.charAt(i);
+        console.log('char is ', char);
+        if(reg.exec(current())){
+            console.log('yay');
+            build += consume();
+            return true;
+        }
+        return false;
+    }
+    for (; i < text.length; i++) {
+        build = "";
+        var curOps = SPLITS;
+        var token = text.charAt(i);
+        if (token in curOps) {
             do {
                 curOps = curOps[token];
+                var is_num = false;
                 if (i+1 == text.length) {
                     break;
                 } else {
                     var next = text.charAt(i+1)
                     if (curOps && token + next in curOps) {
                         token += next;
+                    } else if(is_number(next)){
+                        is_num = true;
+                        break;
                     } else {
                         break;
                     }
                     i++;
                 }
             } while (true);
-            chunked.push(token);
-        } else {
-            if (char in TOKEN_SPLITS) {
-                build = build.trim();
-                if (build != "") {
-                    chunked.push(build);
-                }
-                build = "";
-            } else {
-                build += char;
+            if(!is_num){
+                chunked.push(token);
+                continue;
             }
+        }
+        if (accept_regex(id_begin)){
+            while(accept_regex(id_inside)){}
+            console.log("build is ", build);
+            if (build in Identifiers){
+                chunked.push([build, Kinds[build.toUpperCase()]]);
+            }
+            else{
+                chunked.push([build, Kinds.ID]);
+            }
+        }
+        else if(is_number(token) || accept("+-")){
+
+        } else{
+            chunked.push([token, Kinds.ERR]);
         }
     }
     //TODO we use this piece of code a lot. perhaps make some sort of token buffer class that does this for us
-    build = build.trim();
-    if (build != "") {
-        chunked.push(build);
-    }
     console.log(chunked);
     var length = chunked.length;
     var index = 0;
